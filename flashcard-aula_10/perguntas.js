74263cbe56ab7deb79d2a3724f2105832be2b3de
Daniel162007 <144928536+Daniel162007@users.noreply.github.com>
2024-10-17 10:58:25 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Saiba controlar o seu elixir?',
+    'Todas as cartas possuem certo custo, como também demora algum tempo para que o elixir possa se regenerar. Por isso, para ter uma batalha de sucesso no Clash Royale, é fundamental saber controlá-lo. Para isso, basta atacar seu oponente com cartas que custem uma menor quantidade de elixir do que as utilizadas por ele. Assim, o seu durará mais tempo e você estará mais propenso a derrotar o oponent.
+
+    É fundamental entender esta lógica para poder vencer as partidas, pois, uma vez que seu elixir se esgota, você terá que somente observar enquanto seu oponente lhe ataca — o que não parece muito divertido, certo?.'
+)
+
+criaCartao(
+    'Saiba posicionar suas tropas da maneira correta?',
+    'Caso você posicione suas tropas erroneamente, elas passam a ter mais chances de serem vencidas pelas tropas inimigas. Uma boa estratégia é atrair as tropas oponentes para o centro. Assim, elas ficarão rodeadas pelas suas e a chance de derrotá-las é muito maiorOutra dica interessante é dispersar as suas. Isso mesmo, colocar os homens afastados uns dos outros. Assim, eles não serão atingidos todos ao mesmo tempo, o que permite que você ganhe tempo e possa, assim, vencer o jogo',
+
+)
+
+criaCartao(
+    'Tome cuidado com ofensas excessivas?',
+    'Ser muito ofensivo é quase tão ruim quanto ser muito passivo. Você também precisa se preocupar em defender suas tropas de ataques. Assim, é imprescindível que você não lance todo o seu deck de cartas de uma só vez, mas busque mantê-las junto com o elixir.'
+)
+
+criaCartao(
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
